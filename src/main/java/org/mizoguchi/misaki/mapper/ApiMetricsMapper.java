2d7ceb3282c53060b0f2e122e15f4c15af44a945
Mizoguchi <kiyomizu_wx@qq.com>
2025-12-11 10:15:00 +0800

    Debug

--- src/main/java/org/mizoguchi/misaki/mapper/ApiMetricsMapper.java
+++
@@ -1,8 +0,0 @@
-package org.mizoguchi.misaki.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.mizoguchi.misaki.pojo.entity.ApiMetrics;
-
-public interface ApiMetricsMapper extends BaseMapper<ApiMetrics> {
-
-}
